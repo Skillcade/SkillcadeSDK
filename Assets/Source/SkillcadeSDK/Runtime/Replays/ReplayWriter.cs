40e82e8153172622448383d3a31ab80d1453a538
Aleksandr Litskevich <avlitskevich@gmail.com>
2025-12-21 14:41:16 +0500

    More writer methods

--- Assets/Source/SkillcadeSDK/Runtime/Replays/ReplayWriter.cs
+++ Assets/Source/SkillcadeSDK/Runtime/Replays/ReplayWriter.cs
@@ -17,14 +17,10 @@ namespace SkillcadeSDK.Replays
         
         public int GetSize(IReplayDataObject dataObject) => dataObject.Size + sizeof(int);
 
-        public void WriteInt(int value)
-        {
-            _writer.Write(value);
-        }
-
-        public void WriteLong(long value)
-        {
-            _writer.Write(value);
-        }
+        public void WriteInt(int value) => _writer.Write(value);
+        public void WriteLong(long value) => _writer.Write(value);
+        public void WriteBool(bool value) => _writer.Write(value);
+        public void WriteFloat(float value) =>  _writer.Write(value);
+        public void WriteDouble(double value) =>  _writer.Write(value);
     }
 }
